415538964db5b14292b6c478ee36dd3b2fe36888
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2020-08-18 19:53:29 +0600

    Create 129 (Div.2) A.c

---
+++ Codeforces/129 (Div.2) A.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int i,temp;
+	char arr[100];
+	scanf("%s", &arr);
+	
+	int l = strlen(arr);
+	
+	for(i=2;i<l;) {
+		if(arr[i-2] > arr[i]) {
+			temp = arr[i-2];
+			arr[i-2] = arr[i];
+			arr[i] = temp;
+			
+			i = 2;
+			continue;
+		}
+		i = i + 2;
+	}
+	
+	printf("%s", arr);
+	
+	return 0;
+}
