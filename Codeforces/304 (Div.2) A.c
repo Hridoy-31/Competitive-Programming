50e410cf1539bc66260bc3735c492ac28323ef3f
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2020-08-22 20:43:30 +0600

    Initial commit

---
+++ Codeforces/304 (Div.2) A.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int k,n,w,i,sum=0;
+    scanf("%d %d %d", &k, &n, &w);
+    
+    for (i=1; i<=w; i++) {
+        sum = sum + (k*i);
+    }
+    
+    if (sum<=n) {
+        printf("0");
+    }
+    else {
+        printf("%d", sum-n);
+    }
+    
+    return 0;
+}
