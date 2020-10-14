4b687ac513549cdf4eb460d26ae0e1eb957ab34f
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2020-10-14 20:46:21 +0600

    Initial commit

---
+++ Aizu Online Judge/ITP1/1C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a,b,area,pera;
+  cin >> a;
+  cin >> b;
+
+  area = a*b;
+  pera = 2*(a+b);
+
+  cout << area << ' ' << pera << endl;
+
+  return 0;
+}
