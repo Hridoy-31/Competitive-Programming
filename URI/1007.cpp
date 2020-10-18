28c054b20f4ece6f85dd177c4d62c70380b001bb
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2020-10-19 01:51:53 +0600

    Initial commit

---
+++ URI/1007.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a,b,c,d,r;
+  cin >> a >> b >> c >> d;
+
+  r = ((a*b) - (d*c));
+
+  cout << "DIFERENCA = " << r << endl;
+
+  return 0;
+}
