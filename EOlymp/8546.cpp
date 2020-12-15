e646162d3c4d719ee3e9516d3bdbddd9c28503a3
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2020-12-15 20:00:44 +0600

    Initial commit

---
+++ EOlymp/8546.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    double res = double(n)/(n+1);
+    cout << fixed << setprecision(6) << res << endl;
+
+    return 0;
+}
