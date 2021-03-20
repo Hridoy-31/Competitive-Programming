e9e1bfbff9fc09074ebee7efec86d9d2ad25490c
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2021-03-20 23:54:05 +0600

    upsolve

---
+++ Kattis/herman.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double r;
+    cin >> r;
+    cout << setprecision(8);
+    
+    cout << fixed << M_PI*r*r << endl;
+    cout << fixed << 2*r*r << endl;
+    return 0;
+}
