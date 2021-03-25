84404104006d27e951ce1bc71b76e749b08ff88b
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2021-03-26 01:00:01 +0600

    upsolve

---
+++ Kattis/aaah.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    cin >> s1 >> s2;
+
+    if (s2.size() <= s1.size()) {
+        cout << "go" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
+    return 0;
+}
