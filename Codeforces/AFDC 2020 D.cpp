31f3797fe09589268f574dbb517096766e9c634d
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2021-04-14 02:24:55 +0600

    upsolve

---
+++ Codeforces/AFDC 2020 D.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int s;
+    scanf("%x", &s);
+    if (s%2 == 0) {
+        cout << "0" << endl;
+    }
+    else {
+        cout << "1" << endl;
+    }
+    return 0;
+}
