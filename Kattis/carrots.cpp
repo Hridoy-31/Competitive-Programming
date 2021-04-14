029676e2b091ef916a436741e4630d4118e87886
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2021-04-15 01:07:39 +0600

    upsolve

---
+++ Kattis/carrots.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,p;
+    cin >> n >> p;
+    vector <string> s(n);
+    for (int i=0; i<n; i++) {
+        cin >> s[i];
+    }
+    cout << p << endl;
+    return 0;
+}
