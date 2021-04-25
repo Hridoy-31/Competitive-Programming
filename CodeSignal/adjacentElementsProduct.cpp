c56c4dabf68c3a761532d8839ab51b6c00c6b0be
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2021-04-25 16:01:04 +0600

    function implementation

---
+++ CodeSignal/adjacentElementsProduct.cpp
@@ -0,0 +1,31 @@
+/*
+Given an array of integers, 
+find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+7 and 3 produce the largest product.
+
+Input/Output
+[execution time limit] 0.5 seconds (cpp)
+[input] array.integer inputArra
+An array of integers containing at least two elements.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 10,
+-1000 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+The largest product of adjacent elements.
+*/
+
+int adjacentElementsProduct(vector<int> inputArray) {
+    int mul = inputArray[0]*inputArray[1];
+    for (int i=0; i<inputArray.size()-1; i++) {
+        if (inputArray[i]*inputArray[i+1] > mul) {
+            mul = inputArray[i]*inputArray[i+1];
+        }
+    }
+    return mul;
+}
