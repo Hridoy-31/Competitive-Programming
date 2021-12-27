16e9e2aa9c2a09cefe1a397de31fb78fb7033fd9
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2021-12-28 02:30:34 +0600

    upsolve

---
+++ URI/1014.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int d;
+    float f;
+    cin >> d >> f;
+    cout << fixed << setprecision(3);
+    cout << d/f << " km/l" << endl;
+    return 0;
+}
