ab387f991fcba1f2a85ba74785fb1f091d86dfc0
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2022-07-30 18:00:37 +0600

    upsolve

---
+++ Toph/Add_Them_Up.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+// #include "debugging.h"
+
+using namespace std;
+using ll = long long int;
+
+ll gcd (ll a, ll b) {
+    return a%b==0?b:gcd(b,a%b);
+}
+
+void solve() {
+    ll a,b;
+    cin >> a >> b;
+    cout << a+b << endl;
+}
+
+int main()
+{
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
